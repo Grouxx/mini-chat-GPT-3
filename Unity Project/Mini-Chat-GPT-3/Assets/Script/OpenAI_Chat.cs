c04724d18ddfb24f1506faee95d41007a2c127bd
Groux Antoine <antoine.groux@gadz.org>
2022-03-15 19:50:39 +0100

    memory script

--- Unity Project/Mini-Chat-GPT-3/Assets/Script/OpenAI_Chat.cs
+++ Unity Project/Mini-Chat-GPT-3/Assets/Script/OpenAI_Chat.cs
@@ -6,23 +6,26 @@ using TMPro;
 
 public class OpenAI_Chat : MonoBehaviour
 {
-    private string m_apyKey = "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";
+    private string m_apyKey = "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"; //authentification key
     public TextMeshProUGUI YourInput;
     public Image Neutral;
     public Image Happy;
     public Image Angry;
+
     private void Start()
     {
+        //we set the neutral image.
         Happy.enabled = false;
         Angry.enabled = false;
     }
     public async Task StartAsync()
     {
-        string iBriefing = new string("Is the sentence " + "\"" + YourInput.text + "\"" + " positive? Answer by YES or NO: ");
+        string iBriefing = new string("Is the sentence " + "\"" + YourInput.text + "\"" + " positive? Answer by YES or NO: "); //we use as prompt this string, in wich we put the user's input
 
         var api = new OpenAIAPI(m_apyKey, new Engine("text-davinci-001"));
-        var result = await api.Completions.CreateCompletionAsync(iBriefing, max_tokens: 100, temperature: 0.1f, top_p: 0f);
+        var result = await api.Completions.CreateCompletionAsync(iBriefing, max_tokens: 100, temperature: 0.1f, top_p: 0f); //create the request to GPT-3 and get the completion.
 
+        //we set the image corresponding to the answer of GPT-3
         if (result.ToString().Contains("Yes"))
         {
             Neutral.enabled = false;
@@ -43,6 +46,7 @@ public class OpenAI_Chat : MonoBehaviour
         }
     }
 
+    //we reload a new request every time the user change his input.
     public void Reload(string text)
     {
         var task = StartAsync();
--- Unity Project/Mini-Chat-GPT-3/Assets/Script/OpenAI_Chat_Memory.cs
+++ Unity Project/Mini-Chat-GPT-3/Assets/Script/OpenAI_Chat_Memory.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using OpenAI_API;
+using System.Text;
+using TMPro;
 
 public class OpenAI_Chat_Memory : MonoBehaviour
 {
     private string m_apyKey = "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";
+    public TextMeshProUGUI YourInput;
+    private StringBuilder memory;
+    //public TextMeshProUGUI ResumeTextbox;
+
     public async Task StartAsync()
     {
-        //string iBriefing = new string("Is the sentence " + "\"" + YourInput.text + "\"" + " positive? Answer by YES or NO: ");
 
         var api = new OpenAIAPI(m_apyKey, new Engine("text-davinci-001"));
-        var result = await api.Completions.CreateCompletionAsync("Yes", max_tokens: 100, temperature: 0.1f, top_p: 0f);
+        var result = await api.Completions.CreateCompletionAsync(YourInput.text, max_tokens: 100, temperature: 0.1f, top_p: 0f);
+        //await api.Completions.StreamCompletionAsync(new CompletionRequest("My name is Roger and I am a principal software engineer at Salesforce.  This is my resume:", 200, 0.5, presencePenalty: 0.1, frequencyPenalty: 0.1), res => ResumeTextbox.text += res.ToString());
+        Debug.Log(result);
+        memory.Append(result.ToString());
     }
 
-        // Update is called once per frame
-        void Update()
+    public void AddToMemory(string text)
     {
-        
+        var task = StartAsync();
+        Debug.Log(memory);
     }
 }
